b41a90d10eea20f11bef9461c0c455589fcb8f3b
Nirosha-1910 <84064046+Nirosha-1910@users.noreply.github.com>
2021-05-12 11:59:47 +0530

    Add files via upload

---
+++ prime3.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,i,flag=0;
+	cin>>n;
+	for(i=2;i<=sqrt(n);i++);
+	{
+		if(n%i==0)
+		   flag=1;
+	}
+	if(flag==1)
+	  cout<<"Its not a prime";
+	else
+	 cout<<"Its a prime";
+}
